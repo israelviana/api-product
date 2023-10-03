ebdd1b3994b9e6f3b031d1bc04024d32b85b17f1
israelviana <rael232is@gmail.com>
2023-10-02 21:14:54 -0300

    api-product

---
+++ src/main/java/com/example/apiproduct/models/ProductModel.java
@@ -0,0 +1,47 @@
+package com.example.apiproduct.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "PRODUCTS")
+public class ProductModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id_product;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getId_product() {
+        return id_product;
+    }
+
+    public void setId_product(UUID id_product) {
+        this.id_product = id_product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+
+}
